de0ac42c9b201ccaa98f6822f568a996e2089830
polochvn <polochanin93@mail.ru>
2024-02-12 22:54:38 +0300

    init project

---
+++ src/test/java/com/skillbox/cryptobot/CryptoBotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.skillbox.cryptobot;
+
+import java.util.regex.Pattern;
+
+import org.junit.jupiter.api.Test;
+
+class CryptoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Pattern p = Pattern.compile("[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)");
+		String t = "/subscribe 9999999.12";
+		System.out.println(t.replaceAll("(/subscribe|\\s)", ""));
+	}
+
+}
